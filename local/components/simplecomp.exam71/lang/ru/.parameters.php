6e7c04f9ec1dea906a7a0f82f6ee08ced892c3e1
Roman Smirnov <r.smirnov@citrus-soft.ru>
2024-12-26 17:05:01 +0300

    ex2-71 правки

--- local/components/simplecomp.exam71/lang/ru/.parameters.php
+++ local/components/simplecomp.exam71/lang/ru/.parameters.php
@@ -3,5 +3,5 @@ $MESS["SIMPLECOMP_EXAM2_CAT_IBLOCK_ID"] = "ID инфоблока с катало
 $MESS["SIMPLECOMP_EXAM2_CLASSIFIER_IBLOCK_ID"] = "ID инфоблока с классификатором";
 $MESS["SIMPLECOMP_EXAM2_CLASSIFIER_PROPERTY"] = "Код свойства товара, в котором хранится привязка товара к классификатору";
 $MESS["SIMPLECOMP_EXAM2_DETAIL_PAGE_TEMPLATE"] = "Шаблон ссылки на детальный просмотр товара";
-$MESS["CP_BC_CACHE_GROUPS"] = "Учитывать права доступа";
+$MESS["CP_BNL_CACHE_GROUPS"] = "Учитывать права доступа";
 ?>
