2527c421d7f480bba0c6df96a0e028173fd70660
Roman Smirnov <r.smirnov@citrus-soft.ru>
2024-12-27 11:03:20 +0300

    ex2-100

--- local/components/simplecomp.exam70/lang/ru/component.php
+++ local/components/simplecomp.exam70/lang/ru/component.php
@@ -2,4 +2,5 @@
 $MESS["SIMPLECOMP_EXAM2_IBLOCK_MODULE_NONE"] = "Модуль Информационных блоков не установлен";
 $MESS["PRODUCTS_COUNT"] = "Элементов: #COUNT#";
 $MESS["SIMPLECOMP_EXAM2_UNCORECT_INPUT_PARAMS"] = "Не все параметры введены верно";
+$MESS["IB_ADMIN"] = "ИБ в админке";
 ?>
