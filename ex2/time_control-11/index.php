751f28a5768292d8c003c27d181e7ee9b3bb696c
Roman Smirnov <r.smirnov@citrus-soft.ru>
2024-12-28 12:14:24 +0300

    ex2-88 && ex2-10 && ex2-11

---
+++ ex2/time_control-11/index.php
@@ -0,0 +1,11 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>Самая ресурсоемкая страница: "/products/index.php"&nbsp;<br>
+ <br>
+ Ее доля % в общей статистике нагрузки: 23.58%<br>
+ <br>
+ Компонент выполнение которого занимает больше всего времени: bitrix:catalog.section
+ <br> 
+ Количество запросов: 28
+ <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
